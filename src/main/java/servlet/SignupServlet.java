7cc51f7dc032e6f1b1e730c9dc9354b10b2ac116
Nagapriyadharshini <nagapriyadharshinik@gmail.com>
2025-05-03 14:01:53 +0530

    third commit

--- src/main/java/servlet/SignupServlet.java
+++ src/main/java/servlet/SignupServlet.java
@@ -15,18 +15,19 @@ public class SignupServlet extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		String name = req.getParameter("name");
-		String email = req.getParameter("email");
-		long contact = Long.parseLong(req.getParameter("contact"));
-		String pwd = req.getParameter("pwd");
-		String newPwd = req.getParameter("cnfPwd");
-		
-<<<<<<< HEAD
-=======
-//		((HttpServletResponse) res).sendRedirect("/login");
-		PrintWriter out = res.getWriter();
-		out.println("Sign-up seccess");
->>>>>>> new
+		((HttpServletResponse) res).sendRedirect("/login");
+//		String name = req.getParameter("name");
+//		String email = req.getParameter("email");
+//		long contact = Long.parseLong(req.getParameter("contact"));
+//		String pwd = req.getParameter("pwd");
+//		String newPwd = req.getParameter("cnfPwd");
+//		
+//<<<<<<< HEAD
+//=======
+////		((HttpServletResponse) res).sendRedirect("/login");
+//		PrintWriter out = res.getWriter();
+//		out.println("Sign-up seccess");
+//>>>>>>> new
 	}
 
 }
