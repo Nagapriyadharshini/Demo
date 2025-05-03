c84c4494c5efc3062942570fc8b26cb8c5450745
Nagapriyadharshini <nagapriyadharshinik@gmail.com>
2025-05-03 13:27:23 +0530

    resolved

--- src/main/java/servlet/SignupServlet.java
+++ src/main/java/servlet/SignupServlet.java
@@ -20,11 +20,6 @@ public class SignupServlet extends GenericServlet{
 		String pwd = req.getParameter("pwd");
 		String newPwd = req.getParameter("cnfPwd");
 		
-<<<<<<< HEAD
-		((HttpServletResponse) res).sendRedirect("login");
-=======
-		((HttpServletResponse) res).sendRedirect("/login");
->>>>>>> new
 	}
 
 }
