9a27d9c147568f96a7be9b19712e5f85ca80d328
Nagapriyadharshini <nagapriyadharshinik@gmail.com>
2025-05-03 12:59:27 +0530

    third commit

--- src/main/java/servlet/SignupServlet.java
+++ src/main/java/servlet/SignupServlet.java
@@ -14,6 +14,12 @@ public class SignupServlet extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		long contact = Long.parseLong(req.getParameter("contact"));
+		String pwd = req.getParameter("pwd");
+		String newPwd = req.getParameter("cnfPwd");
+		
 		((HttpServletResponse) res).sendRedirect("login");
 	}
 
